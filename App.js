33011720830aa0cb3badb2db13792c7b589e2ac7
Stuart McCafferty <sm342@hw.ac.uk>
2021-01-14 16:34:59 +0000

    inital commit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import CameraScreen from './screens/CameraScreen';
+import ExploreScreen from './screens/ExploreScreen';
+import FeedScreen from './screens/FeedScreen';
+import LeaderboardScreen from './screens/LeaderboardScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+const FeedStack = createStackNavigator();
+const CameraStack = createStackNavigator();
+const ExploreStack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Feed">
+          {() => (
+            <FeedStack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: '#f5f5f5',
+                },
+                headerTintColor: '#000000',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+              }}
+            >
+              <FeedStack.Screen name="Feed" component={FeedScreen} />
+              <FeedStack.Screen name="Leaderboard" component={LeaderboardScreen} />
+              <FeedStack.Screen name="Profile" component={ProfileScreen} />
+            </FeedStack.Navigator>
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Camera">
+          {() => (
+            <CameraStack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: '#f5f5f5',
+                },
+                headerTintColor: '#000000',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+              }}
+            >
+              <CameraStack.Screen name="Camera" component={CameraScreen} />
+            </CameraStack.Navigator>
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Explore">
+          {() => (
+            <ExploreStack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: '#f5f5f5',
+                },
+                headerTintColor: '#000000',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+              }}
+            >
+              <ExploreStack.Screen name="Explore" component={ExploreScreen} />
+            </ExploreStack.Navigator>
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button
+        title="Reload new profile"
+        onPress={() => navigation.push('Profile')}
+      />
+      <Button
+        title="Go to back to Leaderboard"
+        onPress={() => navigation.navigate('Leaderboard')}
+      />
+
+
+    </View>
+  );
+}

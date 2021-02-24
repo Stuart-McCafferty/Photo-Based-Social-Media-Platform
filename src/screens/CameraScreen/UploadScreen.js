9b6af9600c8bcfcaf07aab3c1ac54ce304ec566a
Husain Mehdi <husmehdi@gmail.com>
2021-02-24 14:07:46 +0000

    Update UploadScreen.js

--- src/screens/CameraScreen/UploadScreen.js
+++ src/screens/CameraScreen/UploadScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, Dimensions } from 'react-native';
 
 export default function FeedScreen({ route, navigation }) {
     const {image} = route.params;
@@ -7,14 +7,17 @@ export default function FeedScreen({ route, navigation }) {
   
     return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {<Image source={{ uri: image }} style={{ width: 375, height: 375 }} />}
+      
+      <View style={{}}>{/*image container */}
+        {<Image source={{ uri: image }} style={{ width: Dimensions.get('window').width, height: 100 }} />}
+      </View>
       <TextInput
       style={{ height: 70, borderColor: 'gray', borderWidth: 1, padding: 20, margin: 30, fontSize: 20 }}
       onChangeText={text => onChangeText(text)}
       value={value}
     />
     <Button
-        title="Upload"
+        title="Uploads"
         color="#004a44"
       />
     </View>

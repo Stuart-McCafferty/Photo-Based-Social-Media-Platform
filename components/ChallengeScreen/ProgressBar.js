0b94ee6333615f65a52a7a0ef6bf669da7831924
Stuart McCafferty <sm342@hw.ac.uk>
2021-03-30 11:18:10 +0100

    ProgressBar Added.  Needs resized

---
+++ components/ChallengeScreen/ProgressBar.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+function ZeroProgress() {
+  return (
+    <View style={{
+              height: 20,
+              width: 50,
+              borderRadius: 25,
+              alignItems: "center",
+              justifyContent: "center",
+              position: "absolute", //Here is the trick
+              bottom: 0,
+              alignSelf: "flex-end"
+              }}>
+      <Image source={require('../../assets/images/0Progress.png')}
+      />
+    </View>
+  );
+}
+
+function ThirtyThreeProgress() {
+  return (
+    <View style={{
+              height: 20,
+              width: 50,
+              borderRadius: 25,
+              alignItems: "center",
+              justifyContent: "center",
+              position: "absolute", //Here is the trick
+              bottom: 0,
+              alignSelf: "flex-end"
+              }}>
+      <Image source={require('../../assets/images/33Progress.png')}
+      />
+    </View>
+  );
+}
+
+function SixtySixProgress() {
+  return (
+    <View style={{
+              height: 20,
+              width: 50,
+              borderRadius: 25,
+              alignItems: "center",
+              justifyContent: "center",
+              position: "absolute", //Here is the trick
+              bottom: 0,
+              alignSelf: "flex-end"
+              }}>
+      <Image source={require('../../assets/images/66Progress.png')}
+      />
+    </View>
+  );
+}
+
+function OneHundredProgress() {
+  return (
+    <View style={{
+              height: 20,
+              width: 50,
+              borderRadius: 25,
+              alignItems: "center",
+              justifyContent: "center",
+              position: "absolute", //Here is the trick
+              bottom: 0,
+              alignSelf: "flex-end"
+              }}>
+      <Image source={require('../../assets/images/100Progress.png')}
+      />
+    </View>
+  );
+}
+
+
+export default function Progressbar({ navigation }) {
+  const noOfCompleted = '1';  //Change this to change the image, will need to link it with back end
+
+    if (noOfCompleted === '0') {
+      return <ZeroProgress />
+    } else if (noOfCompleted === '1') {
+      return <ThirtyThreeProgress />
+    } else if (noOfCompleted === '2') {
+      return <SixtySixProgress />
+    } else if (noOfCompleted === '3') {
+      return <OneHundredProgress />
+    }
+  };

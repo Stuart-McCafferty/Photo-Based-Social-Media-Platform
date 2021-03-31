b27f97adb2a24b526c9fcd77dc76d926c8972b19
Husain Mehdi <husmehdi@gmail.com>
2021-03-31 22:24:46 +0100

    Update Posts.js

--- components/Posts.js
+++ components/Posts.js
@@ -16,7 +16,7 @@ function Posts({ navigation, route }) {
       sourceUser: GLOBAL.USERNAME,
       key: GLOBAL.KEY
     }
-    postMethodFetch(submission, "/api_custom/activity", res => {
+    postMethodFetch(submission, "/api_custom/activity"+GLOBAL.USERNAME, res => {
       updateData(res.activity);
     });
   }, []);
